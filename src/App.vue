0fe3f25d30211bb541009a65753e92211a19e2f6
vol4tim <vol4tim@gmail.com>
2020-02-06 10:13:45 +0400

    step-4
    Signed-off-by: vol4tim <vol4tim@gmail.com>

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,26 @@
 <template>
   <div id="app">
     <h1>Demo dApp</h1>
-    <Demand />
+    <div v-if="isReadyRobonomics">
+      <Demand @onLiability="onLiability" />
+      <div style="display:flex;margin-top:20px;">
+        <div style="width:50%;">
+          <h2>Last demand message</h2>
+          <Json v-if="lastDemand">{{ lastDemand }}</Json>
+        </div>
+        <div style="width:50%;">
+          <h2>Last offer message</h2>
+          <Json v-if="lastOffer">{{ lastOffer }}</Json>
+        </div>
+      </div>
+      <div v-if="liability">
+        <h2>Liability</h2>
+        <Json>{{ liability }}</Json>
+      </div>
+    </div>
+    <div v-else>
+      <h2>load</h2>
+    </div>
   </div>
 </template>
 
@@ -11,11 +30,21 @@ import { init as initRobonomics } from "./utils/robonomics";
 import { init as initWeb3 } from "./utils/web3";
 import { init as initIpfs } from "./utils/ipfs";
 import Demand from "./components/Demand.vue";
+import Json from "./components/Json.vue";
 
 export default {
   name: "app",
   components: {
-    Demand
+    Demand,
+    Json
+  },
+  data() {
+    return {
+      isReadyRobonomics: false,
+      lastDemand: null,
+      lastOffer: null,
+      liability: null
+    };
   },
   async created() {
     const web3 = await initWeb3();
@@ -23,8 +52,34 @@ export default {
     const accounts = await web3.eth.getAccounts();
     Vue.prototype.$robonomics = initRobonomics(web3, ipfs, accounts[0]);
     this.$robonomics.ready().then(() => {
-      // готово
+      this.isReadyRobonomics = true;
+
+      this.$robonomics.onDemand(
+        "QmWDRjU7xrM7pFUDuAVbV6kytuFgooahNLsqvCnipPgSSQ",
+        msg => {
+          this.lastDemand = msg.toObject();
+        }
+      );
+      this.$robonomics.onOffer(
+        "QmWDRjU7xrM7pFUDuAVbV6kytuFgooahNLsqvCnipPgSSQ",
+        msg => {
+          this.lastOffer = msg.toObject();
+        }
+      );
     });
+  },
+  methods: {
+    onLiability(liability) {
+      liability
+        .onResult()
+        .then(() => liability.getInfo())
+        .then(data => {
+          this.liability = { address: liability.address, ...data };
+        });
+      liability.getInfo().then(data => {
+        this.liability = { address: liability.address, ...data };
+      });
+    }
   }
 };
 </script>
--- src/components/Demand.vue
+++ src/components/Demand.vue
@@ -5,7 +5,22 @@
 <script>
 export default {
   methods: {
-    send() {}
+    send() {
+      this.$robonomics
+        .sendDemand({
+          model: "QmWDRjU7xrM7pFUDuAVbV6kytuFgooahNLsqvCnipPgSSQ",
+          objective: "QmYVEPMfzSf2GXhA2heLrnhr931xmnXBEfy79Zrk9Aeskg",
+          token: this.$robonomics.xrt.address,
+          cost: 0,
+          lighthouse: this.$robonomics.lighthouse.address,
+          validator: "0x0000000000000000000000000000000000000000",
+          validatorFee: 0,
+          deadline: 8000000
+        })
+        .then(liability => {
+          this.$emit("onLiability", liability);
+        });
+    }
   }
 };
 </script>
